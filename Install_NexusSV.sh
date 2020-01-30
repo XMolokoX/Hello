ac14cc186d197d905b48bb6d5dca31b8a90057e8
XMolokoX <37104017+XMolokoX@users.noreply.github.com>
2020-01-30 23:09:14 +0700

    Update Install_NexusSV.sh

--- Install_NexusSV.sh
+++ Install_NexusSV.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 cd /./opt
-wget https://sonatype-download.global.ssl.fastly.net/nexus/3/nexus-3.0.2-02-unix.tar.gz
+sudo wget https://sonatype-download.global.ssl.fastly.net/nexus/3/nexus-3.0.2-02-unix.tar.gz
 tar -zxvf nexus-3.0.2-02-unix.tar.gz
 mv /opt/nexus-3.0.2-02 /opt/nexus
-adduser nexus
+sudo adduser nexus
 line=$(sed -n '/root/{/ALL/=}' /etc/sudoers)
 #line = ${cd /./home | ls -al}
-
-
-echo "$line"
-#sed -ie '$line a nexus	ALL=(ALL)	ALL' /etc/sudoers
-sed -ie ''$line' a nexus	ALL=(ALL)	NOPASSWD: ALL' /etc/sudoers 
-chown -R nexus:nexus /opt/nexus
-sed -ie 's/run_as_user=""/run_as_user="nexus"/' /opt/nexus/bin/nexus.rc
-ln -s /opt/nexus/bin/nexus /etc/init.d/nexus
+sudo sed -ie ''$line' a nexus	ALL=(ALL)	NOPASSWD: ALL' /etc/sudoers 
+sudo chown -R nexus:nexus /opt/nexus
+sudo sed -ie 's/run_as_user=""/run_as_user="nexus"/' /opt/nexus/bin/nexus.rc
+sudo ln -s /opt/nexus/bin/nexus /etc/init.d/nexus
 
