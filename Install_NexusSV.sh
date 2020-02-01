4e56a3ce992610781741d2e893f1919f93cf810e
XMolokoX <37104017+XMolokoX@users.noreply.github.com>
2020-02-01 11:50:09 +0700

    Update Install_NexusSV.sh

--- Install_NexusSV.sh
+++ Install_NexusSV.sh
@@ -8,10 +8,10 @@ sudo yum install java-1.8.0-openjdk.x86_64 -y
 cd /./opt
 
 
-sudo wget -O nexus.tar.gz https://download.sonatype.com/nexus/3/latest-unix.tar.gz
+sudo wget -O nexus.tar.gz http://download.sonatype.com/nexus/3/nexus-3.15.1-01-unix.tar.gz
 	
 sudo tar -xvf nexus.tar.gz
-sudo mv /opt/nexus-3* /opt/nexus
+sudo mv /opt/nexus-3* /opt/nexus3
 sudo adduser nexus
 line=$(sudo sed -n '/root/{/ALL/=}' /etc/sudoers)
 #line = ${cd /./home | ls -al}
@@ -20,5 +20,7 @@ sudo chown -R nexus:nexus /opt/nexus
 sudo chown -R nexus:nexus /opt/sonatype-work
 sudo sed -ie 's/run_as_user=""/run_as_user="nexus"/' /opt/nexus/bin/nexus.rc
 sudo ln -s /opt/nexus/bin/nexus /etc/init.d/nexus
+sudo chkconfig --add nexus
 sudo chkconfig nexus on
+
 
