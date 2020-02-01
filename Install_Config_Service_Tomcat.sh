f9bb87b6b71db4f1d2e12ca9db2b8caa70e7beb3
root <root@localhost.localdomain>
2020-02-01 20:48:10 +0700

    123

---
+++ Install_Config_Service_Tomcat.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#sudo yum install epel-release -y
+#sudo yum update -y && sudo reboot
+
+sudo yum install java-1.8.0-openjdk.x86_64 -y
+
+sudo groupadd tomcat
+sudo mkdir -p /opt/tomcat
+sudo useradd -s /bin/nologin -g tomcat -d /opt/tomcat tomcat
+sudo cd ~
+sudo wget https://archive.apache.org/dist/tomcat/tomcat-8/v8.0.33/bin/apache-tomcat-8.0.33.tar.gz
+sudo tar -zxvf apache-tomcat-8.0.33.tar.gz -C /opt/tomcat --strip-components=1
+sudo rm -rf apache*
+sudo cd /opt/tomcat
+sudo chgrp -R tomcat conf
+sudo chmod g+rwx conf
+sudo chmod g+r conf/*
+sudo chown -R tomcat logs/ temp/ webapps/ work/
+
+sudo chgrp -R tomcat bin
+sudo chgrp -R tomcat lib
+sudo chmod g+rwx bin
+sudo chmod g+r bin/*
+
+
+sudo touch /etc/systemd/system/tomcat.service
+sudo cat <<EOT >> /etc/systemd/system/tomcat.service
+[Unit]
+Description=Apache Tomcat Web Application Container
+After=syslog.target network.target
+
+[Service]
+Type=forking
+
+
+ExecStart=/opt/tomcat/bin/startup.sh
+ExecStop=/opt/tomcat/bin/shutdowm.sh
+
+User=tomcat
+Group=tomcat
+
+[Install]
+WantedBy=multi-user.target
+EOT
+
+sudo systemctl start tomcat.service
+sudo systemctl enable tomcat.service
+
+sudo firewall-cmd --zone=public --permanent --add-port=8080/tcp
+sudo firewall-cmd --reload
+
